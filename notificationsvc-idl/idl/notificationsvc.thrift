ec374b4a708dec9a3ebb284acbbbb0e989bb0fd8
bmullins <bmullins@techresidents.com>
2012-09-13 14:55:25 -0400

    Generated IDL dir for notificationsvc

---
+++ notificationsvc-idl/idl/notificationsvc.thrift
@@ -0,0 +1,8 @@
+namespace java com.techresidents.services.notificationsvc.gen
+namespace py trnotificationsvc.gen
+
+include "core.thrift"
+
+service TNotificationService extends core.TRService
+{
+}
