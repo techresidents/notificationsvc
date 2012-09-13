734cac37883cbfdfe1ac8dc3b7d2a1d445f97128
bmullins <bmullins@techresidents.com>
2012-09-13 14:07:24 -0400

    First attempt at notification svc idl.

---
+++ notificationsvc-idl/idl/notificationsvc.thrift
@@ -0,0 +1,58 @@
+namespace java com.techresidents.services.notificationsvc.gen
+namespace py trnotificationsvc.gen
+
+include "core.thrift"
+
+
+/* Exceptions */
+
+exception UnavailableException {
+    1: string fault,
+}
+
+exception InvalidNotificationException {
+    1: string fault,
+}
+
+
+
+/* Notification Priority */
+enum NotificationPriority {
+    LOW_PRIORITY,
+    DEFAULT_PRIORITY,
+    HIGH_PRIORITY,
+}
+
+
+/* Notification */
+struct Notification {
+    1: optional string token,
+    2: NotificationPriority priority,
+    3: optional list<string> recipientEmails,
+    4: optional list<string> recipientUserIds,
+    5: string title,
+    6: optional string htmlText,
+    7: optional string plainText,
+    8: double createdTimestamp,
+}
+
+
+
+/* Service interface */
+
+service TNotificationService extends core.TRService
+{
+    Notification notify(
+            1: string context,
+            2: Notification notification) throws (
+                    1:UnavailableException unavailableException,
+                    2:InvalidNotificationException invalidNotificationException),
+
+    void cancel(
+            1: string token,
+            2: string context) throws (
+                    1:UnavailableException unavailableException,
+                    2:InvalidNotificationException invalidNotificationException),
+
+
+}
