1843fef61fe49fc7431d14ac9deb5573bb1bef8c
bmullins <bmullins@techresidents.com>
2012-09-14 13:43:01 -0400

    Code reviewed thrift interface.

--- notificationsvc-idl/idl/notificationsvc.thrift
+++ notificationsvc-idl/idl/notificationsvc.thrift
@@ -9,7 +9,6 @@ include "core.thrift"
 exception UnavailableException {
     1: string fault,
 }
-
 exception InvalidNotificationException {
     1: string fault,
 }
@@ -24,30 +23,50 @@ enum NotificationPriority {
 }
 
 
-/* Notification */
+/*
+Notification
+   token: optional ID. Used to support cancel() functionality in the Notification Service.
+   context: the request context
+   priority: the notification priority
+   recipientUserIds: list of recipient user IDs to receive this notification
+   subject: notification subject
+   htmlText: notification body in HTML
+   plainText: notification body in plain text
+*/
 struct Notification {
     1: optional string token,
     2: NotificationPriority priority,
-    3: optional list<string> recipientEmails,
-    4: optional list<string> recipientUserIds,
-    5: string title,
-    6: optional string htmlText,
-    7: optional string plainText,
-    8: double createdTimestamp,
+    3: list<string> recipientUserIds,
+    4: string subject,
+    5: optional string htmlText,
+    6: optional string plainText,
 }
 
 
 service TNotificationService extends core.TRService
 {
+    /*
+        Send a notification.
+        Args:
+            context: string representing the request context
+            notification: notification object
+        Returns:
+            The notification object. If no token attribute is
+            provided in the input notification object, the
+            returned object will specify one.
+    */
     Notification notify(
-                1: string context,
-                2: Notification notification) throws (
-                        1:UnavailableException unavailableException,
-                        2:InvalidNotificationException invalidNotificationException),
+        1: string context,
+        2: Notification notification) throws (
+                1:UnavailableException unavailableException,
+                2:InvalidNotificationException invalidNotificationException),
 
-        void cancel(
-                1: string token,
-                2: string context) throws (
-                        1:UnavailableException unavailableException,
-                        2:InvalidNotificationException invalidNotificationException),
+    /*
+    For future.
+    void cancel(
+        1: string token,
+        2: string context) throws (
+                1:UnavailableException unavailableException,
+                2:InvalidNotificationException invalidNotificationException),
+    */
 }
--- notificationsvc/handler.py
+++ notificationsvc/handler.py
@@ -1,15 +1,17 @@
 import logging
 
 from trpycore.thread.util import join
+from trsvcscore.db.models import Notification
 from trsvcscore.service.handler.service import ServiceHandler
-from trpersistsvc.gen import TPersistService
+from trnotificationsvc.gen import TNotificationService
+from trnotificationsvc.gen.ttypes import Notification, NotificationPriority, UnavailableException, InvalidNotificationException
 
 import settings
 from jobmonitor import NotificationJobMonitor
 
 
 
-class NotificationServiceHandler(TPersistService.Iface, ServiceHandler):
+class NotificationServiceHandler(TNotificationService.Iface, ServiceHandler):
     """NotificationServiceHandler manages the notification service.
 
     NotificationServiceHandler is responsible for managing
@@ -36,7 +38,6 @@ class NotificationServiceHandler(TPersistService.Iface, ServiceHandler):
         super(NotificationServiceHandler, self).start()
         self.job_monitor.start()
 
-    
     def stop(self):
         """Stop handler."""
         self.job_monitor.stop()
@@ -46,3 +47,38 @@ class NotificationServiceHandler(TPersistService.Iface, ServiceHandler):
         """Join handler."""
         join([self.job_monitor, super(NotificationServiceHandler, self)], timeout)
 
+
+    def notify(self, requestContext, notification):
+        """Send notification
+
+        Args:
+            requestContext: String to identify calling context
+            notification: Notification object.
+        Returns:
+            If no 'token' attribute was provided in the input
+            notification object, the returned object will
+            specify one.
+        Raises:
+            UnavailableException
+        """
+        try:
+            #TODO verify this is correct
+            db_session = self.get_database_session()
+
+            # Write notification object to db to ensure we don't
+            # lose the notification if this svc goes down.
+
+
+
+
+
+            # Parse notification object, create job for each user
+            # Return notification object with updated Token attribute
+
+
+        except Exception as error:
+            self.log.exception(error)
+            raise UnavailableException(str(error))
+
+        finally:
+            db_session.close()
